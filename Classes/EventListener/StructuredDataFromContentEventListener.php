d9f3f804e56ed70465a64e3cdf650ad652d5908a
harun <harun@web-vision.de>
2026-08-06 13:50:29 +0200

    [BUGFIX] Fix duplicate types, breadcrumbs
    Fix issue with duplicate types getting rendered when using one of the extended types. Added functionality for the breadcrumb URLs to get generated with a user defined URL, which makes it possible for them to get rendered with the Magento URLs instead of the TYPO3 ones. Added functionality for breadcrumbs to be excluded from generation based on the Page backend_layout value

--- Classes/EventListener/StructuredDataFromContentEventListener.php
+++ Classes/EventListener/StructuredDataFromContentEventListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace WebVision\UnitySchema\EventListener;
 
+use Brotkrueml\Schema\Core\Model\TypeInterface;
 use Brotkrueml\Schema\Event\RenderAdditionalTypesEvent;
 use TYPO3\CMS\Core\Routing\PageArguments;
 use WebVision\UnitySchema\Domain\Repository\PageContentRepository;
@@ -37,18 +38,38 @@ final class StructuredDataFromContentEventListener
 
     /**
      * @param array<string, mixed> $contentRecord
+     * @param RenderAdditionalTypesEvent $event
+     * @return void
      */
     private function addTypesForContentRecord(array $contentRecord, RenderAdditionalTypesEvent $event): void
     {
         $isMainEntity = (bool)($contentRecord['tx_schema_is_main_entity'] ?? false);
+
+        foreach ($this->getTypesForContentRecord($contentRecord, $event) as $type) {
+            if ($isMainEntity) {
+                $event->addMainEntityOfWebPage($type);
+                continue;
+            }
+
+            $event->addType($type);
+        }
+    }
+
+    /**
+     * @param array<string, mixed> $contentRecord
+     * @param RenderAdditionalTypesEvent $event
+     * @return array<int, TypeInterface>
+     */
+    private function getTypesForContentRecord(array $contentRecord, RenderAdditionalTypesEvent $event): array
+    {
+        $types = [];
+
         foreach ($this->providerRegistry->findFor($contentRecord) as $provider) {
             foreach ($provider->provide($contentRecord, $event->getRequest()) as $type) {
-                if ($isMainEntity) {
-                    $event->addMainEntityOfWebPage($type);
-                    continue;
-                }
-                $event->addType($type);
+                $types[get_class($type)] = $type;
             }
         }
+
+        return array_values($types);
     }
 }
